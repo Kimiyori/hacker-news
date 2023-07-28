bbd7f5408915ba8e7ec1a033b62d07c1a8b405c5
Maxim <maksimkalin@mail.ru>
2023-07-28 23:42:18 +0300

    feat:add deploy action to remote server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'http://localhost', 'http://wbtestdomain.ru'],
     methods: ['GET'],
   });
   const config = new DocumentBuilder()
