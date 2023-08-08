aa219e59bb538f41e883848d6170809016477302
Maxim <maksimkalin@mail.ru>
2023-08-08 23:52:28 +0300

    add nginx

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost', 'http://wbtestdomain.ru'],
-    methods: ['GET'],
-  });
   const config = new DocumentBuilder()
     .setTitle('Test backend')
     .setDescription('Test backend')
